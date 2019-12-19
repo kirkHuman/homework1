1b40c6af2ee833938612b567321a00b76813ebb2
Volodymyr Postyka <v.postyka@yahoo.com>
2019-12-18 21:33:10 -0800

    first commit

---
+++ random-number.js
@@ -0,0 +1,11 @@
+
+function getRandom() {
+   // console.log(Math.round(Math.random()*100));
+    return Math.round(Math.random()*100);
+}
+
+
+module.exports = getRandom();
+//console.log(getRandom());
+
+
